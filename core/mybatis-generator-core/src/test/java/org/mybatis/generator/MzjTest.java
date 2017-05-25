4b52f9d9f31e6c9160f009e0322e921a414598bd
mzj <mzj313@163.com>
2017-05-25 16:15:05 +0800

    test

---
+++ core/mybatis-generator-core/src/test/java/org/mybatis/generator/MzjTest.java
@@ -0,0 +1,31 @@
+package org.mybatis.generator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class MzjTest {
+
+	//生成到src下了
+	@Test
+	public void getGenFiles() {
+		try {
+			String configFile = "/scripts/genConfig.xml";
+			List<String> warnings = new ArrayList<String>();
+			ConfigurationParser cp = new ConfigurationParser(warnings);
+			Configuration config = cp.parseConfiguration(JavaCodeGenerationTest.class.getResourceAsStream(configFile));
+			    
+			DefaultShellCallback shellCallback = new DefaultShellCallback(true);
+			
+			MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, shellCallback, warnings);
+			myBatisGenerator.generate(null, null, null, true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+}
